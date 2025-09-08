ff46aa361de9f59f74d7a7e40224b377e57f52e6
TanijRoy1 <tanishroy480@gmail.com>
2025-09-08 22:41:11 +0600

    added Our impact section

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ const displayCategories = (catagories) => {
   catagories.forEach((category) => {
     const li = document.createElement("li");
     li.innerHTML = `<li onclick="loadPlants('${category.id}')" id="category-${category.id}"
-                            class="category text-xl font-medium mt-1 py-2.5 px-2 rounded cursor-pointer hover:bg-[#15803D] hover:text-white">
+                            class="category 2xl:text-xl font-medium mt-1 py-2.5 px-2 rounded cursor-pointer hover:bg-[#15803D] hover:text-white">
                             ${category.category_name}
                     </li>`;
     container.appendChild(li);
@@ -33,17 +33,17 @@ const displayPlants = (plants) => {
   container.innerHTML = "";
   plants.forEach((plant) => {
     const div = document.createElement("div");
-    div.innerHTML = `<div class="item rounded-xl bg-white shadow-md p-4 flex flex-col gap-4 h-full">
-                        <img src="${plant.image}" class="w-full h-[300px] rounded-xl object-cover" alt="">
+    div.innerHTML = `<div class="item rounded-xl bg-white shadow-md p-4 flex flex-col gap-2.5 h-full max-h-[450px]">
+                        <img src="${plant.image}" class="w-full h-[200px] rounded-xl object-cover" alt="">
                         <h1 class="text-[#1F2937] font-semibold text-xl cursor-pointer">${plant.name}</h1>
-                        <p class="font-medium text-[#1F2937] flex-1">
+                        <p class="font-medium text-[#1F2937] 2xl:text-[16px] text-[12px] flex-1">
                             ${plant.description}
                         </p>
                         <div class="flex items-center justify-between">
                             <a href="#" class="bg-[#DCFCE7] text-[#15803D] py-1 px-3 font-medium">${plant.category}</a>
                             <p class="font-semibold text-xl">৳<span>${plant.price}</span></p>
                         </div>
-                        <button class="bg-[#15803D] text-white text-xl font-medium py-2 rounded-4xl cursor-pointer">Add
+                        <button class="bg-[#15803D] text-white 2xl:text-xl font-medium py-1.5 rounded-4xl cursor-pointer">Add
                             to Cart</button>
                     </div>`;
     container.appendChild(div);
@@ -67,5 +67,5 @@ const activeClassAdd = (id) => {
 }
 
 
-loadAllPlants();
+// loadAllPlants();
 loadCategories();
